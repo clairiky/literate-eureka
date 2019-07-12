12d84c385343e34237007b4aad25340c67e32f98
Chanchala Roy Lairikyengbam <chanchala.roy.lairikyengbam@intel.com>
2019-07-12 11:06:02 -0700

    Validates the mnist pb model using tensorflow

--- mnist_validate_tf.py
+++ mnist_validate_tf.py
@@ -1,40 +1,32 @@
+
 import tensorflow as tf
-from keras.models import load_model
+import os
 import sys
-import numpy as np  
+from tensorflow.python.platform import gfile
 from PIL import Image
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+import numpy as np
 
-# model = sys.argv[1]
-# input_dir = sys.argv[2]
-# output_dir = sys.argv[3]
-# input_shape = (28, 28, 1)
+model = sys.argv[1]
+input_dir = sys.argv[2]
+output_dir = sys.argv[3]
 
-# #build model
-# model = Sequential()
-# model.add(Conv2D(28, kernel_size=(3,3), input_shape=input_shape))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Flatten()) # Flattening the 2D arrays for fully connected layers
-# model.add(Dense(128, activation=tf.nn.relu))
-# model.add(Dropout(0.2))
-# model.add(Dense(10,activation=tf.nn.softmax))
-# model.compile(optimizer='adam', 
-#               loss='sparse_categorical_crossentropy', 
-#               metrics=['accuracy'])
+sess = tf.Session()
+graph_def = tf.GraphDef()
 
+f = gfile.FastGFile(model, 'rb')
+# Parses a serialized binary message into the current message.
+graph_def.ParseFromString(f.read())
+f.close()
 
+sess.graph.as_default()
+# Import a serialized TensorFlow `GraphDef` protocol buffer
+# and place into the current default `Graph`.
+tf.import_graph_def(graph_def)
 
-# #load model weights
-# model.load_weights("my_mnist3.h5")
-input_dir = sys.argv[1]
-output_dir = sys.argv[2]
-from keras import backend as K
-# This line must be executed before loading Keras model.
-K.set_learning_phase(0)
-from keras.models import load_model
-model = load_model('my_mnist_full.h5')
+#Get the node names
+#print ([n.name for n in sess.graph.as_graph_def().node])
 
+softmax_tensor = sess.graph.get_tensor_by_name('import/dense_2/Softmax:0')
 ref_output = {}
 with open(f'{output_dir}\\result.txt') as f:
     lineList = f.readlines()
@@ -46,12 +38,11 @@ images = {}
 for a in range(100):
     images[f'img_{a}.png'] = Image.open(f'{input_dir}\\img_{a}.png')
 
-final_dict = {}
 top1_accuracy = 0
 for img in images:
-    pred = model.predict(np.array(images[img]).astype('float32').reshape(1,28,28,1))
-    print (pred.argmax())
-    if int(ref_output[img]) == int(pred.argmax()):
+    prediction = sess.run(softmax_tensor, {'import/conv2d_1_input:0': np.array(images[img]).astype('float32').reshape(1,28,28,1)})
+    print (prediction.argmax())
+    if int(ref_output[img]) == int(prediction.argmax()):
         top1_accuracy +=1
 
 print (top1_accuracy/len(images) * 100)
